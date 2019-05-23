df9b318f3027839429a07434eabaff663ca8d904
GabrielKrysa <gabrielkrysa@gmail.com>
2019-05-23 15:04:02 -0300

    Implementada as rotas de login e logout

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,42 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/', function () {
+    return view('index');
+})->name('principal.index');
+
+Route::get('/cadastro', function () {
+    return view('registro');
+})->name('cadastro');
+
+Route::get('/entrar', function () {
+    return view('login');
+})->name('login');
+
+Route::get('/sobre', function () {
+    return view('about');
+})->name('sobre');
+
+Route::get('/cadastrapropriedade', function () {
+    return view('propriedade');
+})->name('propriedade');
+
+Route::get('/recuperarsenha', function () {
+    return view('senha');
+})->name('recuperaSenha');
+
+Route::get('/relatorio', function () {
+    return view('relatorio');
+})->name('relatorio');
+
+Route::get('/contato', function () {
+    return view('mail');
+})->name('contato');
+
+
+Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/relatorio', 'HomeController@index')->name('relatorio');
+Route::post('/login', 'Auth\LoginController@login')->name('login');
 
