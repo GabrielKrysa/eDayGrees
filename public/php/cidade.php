0e24f554ad9a64fb28172b3df106a06d65f7aa7b
GabrielKrysa <gabrielkrysa@gmail.com>
2019-05-29 19:00:03 -0300

    configuracao script das cidades

--- public/php/cidade.php
+++ public/php/cidade.php
@@ -18,8 +18,9 @@ while ($dado = $result->fetch_array()) {
     $nome[] = utf8_encode($dado['nome']);
 }
 
-
-for ($i = 0; $i < count($id); $i++) {
+$html = "<option value='0'>Selecionar cidade</option>";
+echo($html);
+for ($i = 1; $i < count($id); $i++) {
     $html = "<option value='" . $id[$i] . "'>" . $nome[$i] . "</option>";
     echo($html);
 }
