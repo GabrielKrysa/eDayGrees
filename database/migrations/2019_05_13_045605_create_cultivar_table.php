4ffc3f01a400aa9ef988858253bc90cf3e065cb1
Mark Geffer <markgeffer@hotmail.com>
2019-06-05 15:33:22 -0300

    atualizacao das tabelas de cultivar, adicionando atributos

--- database/migrations/2019_05_13_045605_create_cultivar_table.php
+++ database/migrations/2019_05_13_045605_create_cultivar_table.php
@@ -17,7 +17,14 @@ class CreateCultivarTable extends Migration
             $table->bigIncrements('id'); // chave primária dentro do sistema
             $table->string('cultivar');  // nome da cultiva para cadastro
             $table->string('ciclo'); // clide de cada cultivar
-            $table->double('temperatura_base'); // temperatura base da minha cultivar que está sendo cadastrada 
+            $table->Integer('temperatura_base'); // temperatura base da minha cultivar que está sendo cadastrada 
+            $table->Integer('gemaAlgodao'); // temperatura cadastrada dentro do sistema para cada cultivar, usada para fazer verificacoes para possiveis mudancas de estadios fenologicos
+            $table->Integer('brotacao'); // ||
+            $table->Integer('florescimento'); // ||
+            $table->Integer('aparecimentoInflorescencia'); // ||
+            $table->Integer('inicioMaturacao'); // ||
+            $table->Integer('colheita'); // ||
+
         });
     }
 
