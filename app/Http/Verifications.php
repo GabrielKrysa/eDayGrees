0f8eb0f213b20726389933ee3745c33b9dc4305b
GabrielKrysa <gabrielkrysa@gmail.com>
2019-06-05 11:28:50 -0300

    Implementada classe de verificação e metodo de verificação de CPF

--- app/Http/Verifications.php
+++ app/Http/Verifications.php
@@ -6,49 +6,45 @@ namespace App\Http;
 
 class Verifications
 {
-    function verificaCPF($cpf)
+    function validaCPF($cpf = null)
     {
-        function validaCPF($cpf = null)
-        {
 
-            if (empty($cpf)) {
-                return false;
-            }
+        if (empty($cpf)) {
+            return false;
+        }
 
-            $cpf = preg_replace("/[^0-9]/", "", $cpf);
-            $cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT);
+        $cpf = preg_replace("/[^0-9]/", "", $cpf);
+        $cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT);
 
-            dd($cpf);
 
-            if (strlen($cpf) != 11) {
-                return false;
-            }
-            else if ($cpf == '00000000000' ||
-                $cpf == '11111111111' ||
-                $cpf == '22222222222' ||
-                $cpf == '33333333333' ||
-                $cpf == '44444444444' ||
-                $cpf == '55555555555' ||
-                $cpf == '66666666666' ||
-                $cpf == '77777777777' ||
-                $cpf == '88888888888' ||
-                $cpf == '99999999999') {
-                return false;
-            } else {
+        if (strlen($cpf) != 11) {
+            return false;
+        }
+        else if ($cpf == '00000000000' ||
+            $cpf == '11111111111' ||
+            $cpf == '22222222222' ||
+            $cpf == '33333333333' ||
+            $cpf == '44444444444' ||
+            $cpf == '55555555555' ||
+            $cpf == '66666666666' ||
+            $cpf == '77777777777' ||
+            $cpf == '88888888888' ||
+            $cpf == '99999999999') {
+            return false;
+        } else {
 
-                for ($t = 9; $t < 11; $t++) {
+            for ($t = 9; $t < 11; $t++) {
 
-                    for ($d = 0, $c = 0; $c < $t; $c++) {
-                        $d += $cpf{$c} * (($t + 1) - $c);
-                    }
-                    $d = ((10 * $d) % 11) % 10;
-                    if ($cpf{$c} != $d) {
-                        return false;
-                    }
+                for ($d = 0, $c = 0; $c < $t; $c++) {
+                    $d += $cpf{$c} * (($t + 1) - $c);
                 }
-
-                return true;
+                $d = ((10 * $d) % 11) % 10;
+                if ($cpf{$c} != $d) {
+                    return false;
+                }
             }
+
+            return true;
         }
     }
 }
